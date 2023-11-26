5a78420b082fea3be84db34397c0090d23015cfb
Kirill <kirillteplycov888@gmail.com>
2023-11-26 17:54:27 +0500

    Выполненная работа на оценку 3. Тепляков

--- ApplicationSettings_Тепляков/Pages/Settings.xaml.cs
+++ ApplicationSettings_Тепляков/Pages/Settings.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,24 +23,47 @@ namespace ApplicationSettings_Тепляков.Pages
     /// </summary>
     public partial class Settings : Page
     {
-        public Settings()
+        public MainWindow mainWindow;
+        System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
+        ColorDialog colorDialog = new ColorDialog();
+        
+
+        public Settings(MainWindow _main)
         {
             InitializeComponent();
+            mainWindow = _main;
+            ofd.InitialDirectory = "C:\\";
+            ofd.Filter = "Access file (*.accdb) | *.accdb | All files (*.*) | *.*";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+            colorDialog.AllowFullOpen = true;
+            colorDialog.ShowHelp = false;
         }
 
         private void OpenDataBase(object sender, RoutedEventArgs e)
         {
-
+            if(ofd.ShowDialog() == DialogResult.OK)
+            tb_database.Text = ofd.FileName;    
         }
 
         private void SelectScreenresolution(object sender, SelectionChangedEventArgs e)
         {
-
+            System.Windows.Controls.ComboBox cB = sender as System.Windows.Controls.ComboBox;
+            TextBlock tB = cB.SelectedValue as TextBlock;
+            string resolution = tB.Text;
+            string[] separator = new string[1] {" x "};
+            mainWindow.Width = int.Parse(resolution.Split(separator, StringSplitOptions.None)[0]);
+            mainWindow.Height = int.Parse(resolution.Split(separator, StringSplitOptions.None)[1]);
         }
 
         private void SelectColorApplication(object sender, RoutedEventArgs e)
         {
-
+            if(colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                System.Drawing.Color color = colorDialog.Color;
+                gr_header.Background = new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
+                gr_application.Background = new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
+            }
         }
 
         private void SelectColorText(object sender, RoutedEventArgs e)
